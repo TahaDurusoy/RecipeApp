7d1dc3a30de23edc5576efb696962ff8d53cdad4
TahaDurusoy <tahadurusoy@ogr.bandirma.edu.tr>
2024-09-30 18:27:00 +0300

    firstcommit

---
+++ app/src/main/java/com/example/recipeapp/Adapters/RandomRecipeAdapter.java
@@ -0,0 +1,75 @@
+package com.example.recipeapp.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recipeapp.Listeners.RecipeClickListener;
+import com.example.recipeapp.Models.Recipe;
+import com.example.recipeapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RandomRecipeAdapter extends RecyclerView.Adapter<RandomRecipeViewHolder>{
+    Context context;
+    List<Recipe> list;
+    RecipeClickListener listener;
+
+    public RandomRecipeAdapter(List<Recipe> list, Context context, RecipeClickListener listener) {
+        this.list = list;
+        this.context = context;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public RandomRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new RandomRecipeViewHolder(LayoutInflater.from(context).inflate(R.layout.list_random_recipe, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RandomRecipeViewHolder holder, int position) {
+        holder.textView_tittle.setText(list.get(position).title);
+        holder.textView_tittle.setSelected(true);
+        holder.textView_likes.setText(list.get(position).aggregateLikes+" Likes");
+        holder.textView_servings.setText(list.get(position).servings+" Servings");
+        holder.textView_time.setText(list.get(position).readyInMinutes+" Minutes");
+        Picasso.get().load(list.get(position).image).into(holder.imageView_food);
+
+        holder.random_list_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onRecipeClicked(String.valueOf(list.get(holder.getAdapterPosition()).id));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
+
+class RandomRecipeViewHolder extends RecyclerView.ViewHolder {
+    CardView random_list_container;
+    TextView textView_tittle, textView_servings, textView_likes, textView_time;
+    ImageView imageView_food;
+
+    public RandomRecipeViewHolder(@NonNull View itemView) {
+        super(itemView);
+        random_list_container = itemView.findViewById(R.id.random_list_container);
+        textView_tittle = itemView.findViewById(R.id.textView_tittle);
+        textView_likes = itemView.findViewById(R.id.textView_likes);
+        textView_time = itemView.findViewById(R.id.textView_time);
+        textView_servings = itemView.findViewById(R.id.textView_servings);
+        imageView_food = itemView.findViewById(R.id.imageView_food);
+    }
+}
